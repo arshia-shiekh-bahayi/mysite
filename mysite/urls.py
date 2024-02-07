b4c921fe7662b58a70a7c3587e0913530dbf83e3
arshia-shiekh-bahayi <arshiasheikhbahayi@gmail.com>
2024-02-07 13:36:20 +0330

    Update urls.py
    just added a test veiw

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from mysite.views import http_test
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('http-test',http_test)
 ]
