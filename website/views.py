0cd167362e4c69fcf022dabf0160dcd934903200
arshia-shiekh-bahayi <arshiasheikhbahayi@gmail.com>
2024-02-07 14:13:49 +0330

    Update views.py
    added the defs

--- website/views.py
+++ website/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse, JsonResponse
+def http_test(request):
+    return HttpResponse('<h1>this is a test<h1>')
+def json_test(request):
+    return JsonResponse({'name':'ali'})
