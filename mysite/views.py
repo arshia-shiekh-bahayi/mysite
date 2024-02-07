5a8c9116fc6c4fa3649988fa2b8c11bd9ab3b850
arshia-shiekh-bahayi <arshiasheikhbahayi@gmail.com>
2024-02-07 13:41:36 +0330

    Update views.py
    created json view

--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,5 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 def http_test(request):
     return HttpResponse('<h1>this is a test<h1>')
+def json_test(request):
+    return JsonResponse({'name':'ali'})
