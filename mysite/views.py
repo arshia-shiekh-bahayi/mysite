73da752d0c5c68251304630b1091f41db24edeb6
arshia-shiekh-bahayi <arshiasheikhbahayi@gmail.com>
2024-02-07 14:13:01 +0330

    Delete views.py
    deleted it to make a new one in an app

--- mysite/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse, JsonResponse
-def http_test(request):
-    return HttpResponse('<h1>this is a test<h1>')
-def json_test(request):
-    return JsonResponse({'name':'ali'})
