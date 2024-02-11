d25b9a6f77b88fa7c2ea6cd0bf368de172507353
arshia-shiekh-bahayi <arshiasheikhbahayi@gmail.com>
2024-02-11 18:33:08 +0330

    made it static

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 def index_view(request):
-    return render(request,'website\index.html')
+    return render(request,'website/index.html')
 def about_view(request):
         return render(request,'website/about.html')
 def contact_view(request):
