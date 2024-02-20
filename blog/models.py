2f74444fa437bb2ff1db8e582bf7f39b30f28607
arshia-shiekh-bahayi <arshiasheikhbahayi@gmail.com>
2024-02-20 19:29:59 +0330

    migrating and making  models

--- blog/models.py
+++ blog/models.py
@@ -4,3 +4,13 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
+    # img 
+    # author
+    # tag 
+    # category 
+    counted_views = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(null=True)
+    ceated_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    
