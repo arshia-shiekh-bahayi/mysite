0fc50c4b63f00d9ef58b425270f6b45d886b2a44
arshia-shiekh-bahayi <arshiasheikhbahayi@gmail.com>
2024-02-22 10:09:17 +0330

    changing the name

--- blog/models.py
+++ blog/models.py
@@ -11,6 +11,6 @@ class Post(models.Model):
     counted_views = models.IntegerField(default=0)
     status = models.BooleanField(default=False)
     published_date = models.DateTimeField(null=True)
-    ceated_date = models.DateTimeField(auto_now_add=True)
+    created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
     
