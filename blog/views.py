b107168d24c77b073b38005e08b9bd7e639a5d5a
arshia-shiekh-bahayi <arshiasheikhbahayi@gmail.com>
2024-02-24 20:20:51 +0330

    rendered test

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-
+from blog.models import Post
 # Create your views here.
 def blog_view(request):
     return render(request,'blog/blog-home.html')
 def blog_single(request):
     return render(request,'blog/blog-single.html')
+def test(request):
+    posts = Post.objects.all()
+    context = {'posts':posts}
+    return render(request,'test.html',context)
