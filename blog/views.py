9d34393414c48e6132b68055d7df4d58a0ac6fbf
arshia-shiekh-bahayi <arshiasheikhbahayi@gmail.com>
2024-02-25 10:43:51 +0330

    test html
    like i said

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
 from blog.models import Post
+from django.utils import timezone
 # Create your views here.
 def blog_view(request):
-    return render(request,'blog/blog-home.html')
-def blog_single(request):
-    return render(request,'blog/blog-single.html')
-def test(request):
-    posts = Post.objects.all()
+    posts = Post.objects.filter(status =1)
     context = {'posts':posts}
+    return render(request,'blog/blog-home.html',context)
+def blog_single(request,pid):
+    posts = Post.objects.filter(status =1)
+    post = get_object_or_404(posts, pk=pid) 
+    context = {'post':post}
+    return render(request,'blog/blog-single.html',context)
+def test(request,pid):
+    #post = Post.objects.get(id=pid)
+    post = get_object_or_404(Post, pk=pid) 
+    context = {'post':post}
     return render(request,'test.html',context)
+def past_post_list(request):
+    past_posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    return render(request, 'E:/Django course/mysite/templates/blog/past-posts.html', {'past_posts': past_posts})
