0d747e5ae6a316ebbf1c493d80c69edf051f6a28
arshia-shiekh-bahayi <arshiasheikhbahayi@gmail.com>
2024-02-25 10:41:38 +0330

    added some other list_display methods

--- blog/admin.py
+++ blog/admin.py
@@ -4,8 +4,8 @@ from blog.models import Post
 class PostAdmin(admin.ModelAdmin):
   date_hierarchy = 'created_date'
   empty_value_display = '-empty-'
-  list_display = ('title','counted_views','status','published_date','created_date' )
-  list_filter = ('status',)
+  list_display = ('author','title','counted_views','status','published_date','created_date' )
+  list_filter = ('status','author')
   #ordering = ['-created_date']
   search_fields =  ['title', 'content']
 admin.site.register(Post,PostAdmin)
