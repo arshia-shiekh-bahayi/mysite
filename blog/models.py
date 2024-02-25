bf09b35e0f6157ad11229dc982b2e5b6a2e32411
arshia-shiekh-bahayi <arshiasheikhbahayi@gmail.com>
2024-02-25 10:42:41 +0330

    imported User

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Post(models.Model):
+    image = models.ImageField(upload_to='blog/', default='blog/default.jpg') 
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=255)
     content = models.TextField()
-    # img 
-    # author
     # tag 
     # category 
     counted_views = models.IntegerField(default=0)
@@ -14,7 +14,7 @@ class Post(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
     
-    
+
     class Meta:
         ordering = ["created_date",]
     def __str__(self):
