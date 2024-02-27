d1840b89eef161f47c8842571b3664824f79e086
arshia-shiekh-bahayi <arshiasheikhbahayi@gmail.com>
2024-02-27 18:27:41 +0330

    paste all my testing methods of view in other defs

--- blog/views.py
+++ blog/views.py
@@ -3,16 +3,13 @@ from blog.models import Post
 from django.utils import timezone
 # Create your views here.
 def blog_view(request):
-    posts = Post.objects.filter(status =1)
+    posts = Post.objects.filter(status=1 ,published_date__lte=timezone.now()).order_by('-published_date')
     context = {'posts':posts}
     return render(request,'blog/blog-home.html',context)
 def blog_single(request,pid):
-    posts = Post.objects.filter(status =1)
+    posts = Post.objects.filter(status =1,published_date__lte=timezone.now()).order_by('-published_date')
     post = get_object_or_404(posts, pk=pid) 
     post.counted_views += 1
     post.save()  
     context = {'post':post}
     return render(request,'blog/blog-single.html',context)
-def past_post_list(request):
-    past_posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-    return render(request, 'E:/Django course/mysite/templates/blog/past-posts.html', {'past_posts': past_posts})
