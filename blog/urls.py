176c222e30e1e01cbb909b4facd086fff0447e33
arshia-shiekh-bahayi <arshiasheikhbahayi@gmail.com>
2024-02-29 10:51:27 +0330

    added prev post and next post option

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,12 @@
 from django.urls import path
 from blog.views import *
 from . import views
-
 app_name = 'blog'
 urlpatterns = [
     path('', blog_view , name='index'),
     path('<int:pid>', blog_single , name='single'),
     path('prev/<int:pid>', prev_post , name='prev_post'),
-    path('next/<int:pid>', next_post , name='next_post'),
+    path('next<int:pid>', next_post , name='next_post'),
 
 
 ]
