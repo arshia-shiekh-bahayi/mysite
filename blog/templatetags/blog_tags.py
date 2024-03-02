f5138e45989da22e35b2d75ec82c46fb3277d750
arshia-shiekh-bahayi <arshiasheikhbahayi@gmail.com>
2024-03-02 18:51:04 +0330

    Create blog_tags.py

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,11 @@
+from django import template
+from blog.models import Post
+register = template.Library()
+@register.simple_tag(name='totalposts')
+def function():
+    posts = Post.objects.filter(status=1).count()
+    return posts
+@register.simple_tag(name='posts')
+def function():
+    posts = Post.objects.filter(status=1)
+    return posts
