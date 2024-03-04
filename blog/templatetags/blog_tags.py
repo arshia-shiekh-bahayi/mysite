491f88d4b5549e3044f8948a83ea1d70855ab4fa
arshia-shiekh-bahayi <arshiasheikhbahayi@gmail.com>
2024-03-04 16:21:58 +0330

    Update blog_tags.py

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from blog.models import Post
+from django.shortcuts import get_object_or_404
 register = template.Library()
 @register.simple_tag(name='totalposts')
 def function():
@@ -9,3 +10,13 @@ def function():
 def function():
     posts = Post.objects.filter(status=1)
     return posts
+@register.simple_tag(name='prev_post')
+def function(pid):
+    posts = Post.objects.filter(status=1)
+    post  = get_object_or_404(posts , pk= pid-1)
+    return post
+@register.simple_tag(name='next_post')
+def function(pid):
+    posts = Post.objects.filter(status=1)
+    post  = get_object_or_404(posts , pk= pid+1)
+    return post
