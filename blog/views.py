44cb54d23c60ec88cb0c36f86d2a09fbae0bd76b
arshia-shiekh-bahayi <arshiasheikhbahayi@gmail.com>
2024-03-04 18:09:19 +0330

    made it show the next and previous posts

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render , get_object_or_404
 from blog.models import Post
 from django.utils import timezone
+
 # Create your views here.
 def blog_view(request):
     posts = Post.objects.filter(status=1 ,published_date__lte=timezone.now()).order_by('-published_date')
@@ -11,21 +12,15 @@ def blog_single(request,pid):
     post = get_object_or_404(posts, pk=pid) 
     post.counted_views += 1
     post.save()  
-    context = {'post':post}
-    return render(request,'blog/blog-single.html',context)
-def prev_post(request,pid):
-    posts = Post.objects.filter(status=1 ,published_date__lte=timezone.now()).order_by('-published_date')
-    post = get_object_or_404(posts, pk = pid-1)
-    post.counted_views +=1 
-    post.save()
-    context = {'post':post}
-    return render(request,'blog/blog-single.html',context)
-def next_post(request,pid):
-    posts = Post.objects.filter(status=1 ,published_date__lte=timezone.now()).order_by('-published_date')
-    post = get_object_or_404(posts, pk = pid+1)
-    post.counted_views +=1 
-    post.save()
-    context = {'post':post}
+    if Post.objects.latest('id')!=post:
+     next_post = get_object_or_404(posts , pk=pid+1)
+    else :
+       next_post= None
+    if post.id != 1 :
+     prev_post = get_object_or_404(posts , pk=pid-1)
+    else :
+       prev_post = None
+    context = {'post':post,'prev_post':prev_post,'next_post':next_post}
     return render(request,'blog/blog-single.html',context)
 def test(request):
     return render(request,'test.html')
