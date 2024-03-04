206f13c981638849ac687251000e4192f7477263
arshia-shiekh-bahayi <arshiasheikhbahayi@gmail.com>
2024-03-04 18:09:27 +0330

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -5,8 +5,6 @@ app_name = 'blog'
 urlpatterns = [
     path('', blog_view , name='index'),
     path('<int:pid>', blog_single , name='single'),
-    path('<int:pid>-1', prev_post , name='prev_post'),
-    path('<int:pid>+1', next_post , name='next_post'),
     path('test', test , name='test'),
 
 
