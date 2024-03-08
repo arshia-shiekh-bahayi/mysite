e388650598bc8354de72b05336ee80bac64c111e
arshia-shiekh-bahayi <arshiasheikhbahayi@gmail.com>
2024-03-08 10:00:48 +0330

    Update blog_tags.py

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -14,9 +14,9 @@ def function():
 @register.filter
 def snippet(value,arg=20):
     return value[:arg]+'...'
-@register.inclusion_tag("popularposts.html")
-def popularposts():
-    posts = Post.objects.filter(status=1).order_by('published_date')[:3]
+@register.inclusion_tag("blog/popularposts.html")
+def latestposts(arg=3):
+    posts = Post.objects.filter(status=1).order_by('published_date')[:arg]
     return {"posts": posts}
 
 
