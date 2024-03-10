1b4f60f548adf2761f8336756aece6915b00cc4a
arshia-shiekh-bahayi <arshiasheikhbahayi@gmail.com>
2024-03-10 15:11:50 +0330

    .

--- website/templatetags/website_tags.py
+++ website/templatetags/website_tags.py
@@ -1,8 +1,11 @@
 from django import template
 from blog.models import Post , Category
+from django.utils import timezone
+
 register = template.Library()
 @register.inclusion_tag("website/latestposts.html")
 def latestposts(arg=6):
-    posts = Post.objects.filter(status=1).order_by('-published_date')[:arg]
+    now = timezone.now()
+    posts = Post.objects.filter(status=1,published_date_lt=now).order_by('-published_date')[:arg]
     return {"posts": posts}
 
--- website/views.py
+++ website/views.py
@@ -2,14 +2,28 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from blog.models import Post
 from django.core.paginator import Paginator
+from website.models import Contact
 def index_view(request,arg=6):
     posts = Post.objects.filter(status=1).order_by('-published_date')[:arg]
     posts = Paginator(posts,2)
     context = {'posts': posts}
     return render(request,'website/index.html',context)
-
 def about_view(request):
         return render(request,'website/about.html')
 def contact_view(request):
         return render(request,'website/contact.html')
+def test_view(request):
+       if request.method == 'POST':
+              name = request.POST.get('name')
+              email = request.POST.get('email')
+              subject = request.POST.get('subject')
+              message = request.POST.get('message')
+              print(name,email,subject,message)
+              c = Contact()
+              c.name=name
+              c.email=email
+              c.subject=subject
+              c.message=message
+              c.save()
+       return render(request,'test.html',{})
 
