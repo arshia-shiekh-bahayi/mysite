299c436414a586bc798fbb8c304559eae4b7b52d
arshia-shiekh-bahayi <arshiasheikhbahayi@gmail.com>
2024-03-19 13:14:12 +0330

    added rss feed

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from blog.views import *
 from . import views
+from blog.feeds import LatestEntriesFeed
 app_name = 'blog'
 urlpatterns = [
     path('', blog_view , name='index'),
@@ -9,6 +10,7 @@ urlpatterns = [
     path('tag/<str:tag_name>', blog_view , name='tag'),
     path('author/<str:author_username>', blog_view , name='author'),
     path('search/', blog_search , name='search'),
+    path('rss/feed', LatestEntriesFeed()),
     path('test', test , name='test'),
 
 
