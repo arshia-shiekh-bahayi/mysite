5c0346c97e835f73c6418dcbb4210fc488f05dd3
arshia-shiekh-bahayi <arshiasheikhbahayi@gmail.com>
2024-03-22 19:57:40 +0430

    added login

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,5 @@ app_name = 'accounts'
 urlpatterns = [
     path('login', views.login_view, name='login'),
     #path('logout', views.logout_view, name='logout'),
-    path('singup', views.singup_view, name='singup'),
+    path('signup', views.signup_view, name='signup'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,24 @@
-from django.shortcuts import render
-
+from django.shortcuts import render , redirect
+from django.contrib.auth import authenticate , login
+from django.contrib.auth.forms import AuthenticationForm
 # Create your views here.
 def login_view(request):
-    if request.user.is_authenticated:
-        msg = f'You are already logged in as {request.user.username}'
+    if not request.user.is_authenticated:  
+     if request.method == 'POST':
+      form = AuthenticationForm(request=request ,data=request.POST)
+      if form.is_valid():
+       username = form.cleaned_data.get('username')
+       password = form.cleaned_data.get('password')
+       user = authenticate(request , username=username , password=password)
+      if user is not None :
+         login(request, user)
+         return redirect('/')
+     form = AuthenticationForm()
+     context = {'form': form}
+     return render(request, 'accounts/login.html', context)
     else:
-        msg = 'You are not logged in'
-    return render(request, 'accounts/login.html', {'msg': msg})
+      return redirect('/')
 #def logout_view(request):
 #   return render(request, 'logout.html')
-def singup_view(request):
-    return render(request,'accounts/singup.html')
+def signup_view(request):
+    return render(request,'accounts/signup.html')
