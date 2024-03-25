c420f1dc5b2498ddd3dcccb8b2a7a7d56a3e792d
arshia-shiekh-bahayi <arshiasheikhbahayi@gmail.com>
2024-03-25 14:05:12 +0430

    added singup

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render , redirect
 from django.contrib.auth import authenticate , login , logout
-from django.contrib.auth.forms import AuthenticationForm
-
+from django.contrib.auth.forms import AuthenticationForm , UserCreationForm
+from django.urls import reverse
 # Create your views here.
 def login_view(request):
     if not request.user.is_authenticated:  
@@ -29,4 +29,14 @@ def logout_view(request):
 
 
 def signup_view(request):
-    return render(request,'accounts/signup.html')
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return render(request, 'accounts/login.html')
+        form = UserCreationForm()
+        context = {'form':form}
+        return render(request,'accounts/signup.html',context)
+    else:
+        return redirect('/')
